6d8c17cef33299315dbf846c2da3dc02ce8dbf86
CatiMira <CatiiMira@gmail.com>
2016-06-29 10:25:10 +0200

    Fuck yeah hoch 2

---
+++ app/src/main/java/com/example/bayrec/chuchutimer/ShowAlarm.java
@@ -0,0 +1,13 @@
+package com.example.bayrec.chuchutimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_alarm);
+    }
+}
