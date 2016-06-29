d9d95d640de9f2eed1b170bada4e331e9305f0d0
DeusArrow <dmytriy.pelts@bwdbern.ch>
2016-06-29 13:34:41 +0200

    Biggest change alive

--- app/src/main/java/com/example/bayrec/chuchutimer/MainActivity.java
+++ app/src/main/java/com/example/bayrec/chuchutimer/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.bayrec.chuchutimer;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createNewAlarm(View v){
+        Intent newWindow = new Intent(MainActivity.this, SelectTrain.class);
+        this.startActivity(newWindow);
     }
 }
--- app/src/main/java/com/example/bayrec/chuchutimer/SelectTrain.java
+++ app/src/main/java/com/example/bayrec/chuchutimer/SelectTrain.java
@@ -1,13 +1,93 @@
 package com.example.bayrec.chuchutimer;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class SelectTrain extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Timer;
+
+public class SelectTrain extends AppCompatActivity
+{
+    private int year, month, day;
+    private int hour, minute;
+    // static final int DIALOG_ID = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_train);
+
+        final Calendar cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH);
+        day = cal.get(Calendar.DAY_OF_MONTH);
+
+        final Time now = new Time();
+        now.setToNow();
+        hour = now.hour;
+        minute = now.minute;
+
+        UpdateDate(year, month, day);
+        UpdateTime(hour, minute);
+    }
+
+    public void createCalendar(View v)
+    {
+        showDialog(0);
+    }
+
+    public void createTime(View v)
+    {
+        showDialog(1);
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int thisYear, int monthOfYear, int dayOfMonth)
+        {
+            UpdateDate(thisYear, monthOfYear, dayOfMonth);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener tpickerListner
+            = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
+            UpdateTime(hourOfDay, minute);
+        }
+    };
+
+    protected Dialog onCreateDialog(int id)
+    {
+        if (id == 0)
+            return new DatePickerDialog(this, dpickerListner, year, month, day);
+        if(id == 1)
+            return new TimePickerDialog(this, tpickerListner, hour, minute, true);
+
+        return null;
+    }
+
+    private void UpdateDate(int year, int month, int day)
+    {
+        TextView tv = (TextView) findViewById(R.id.DateTextEditor);
+        tv.setText(day + "." + month + "." +year);
+    }
+
+    private void UpdateTime(int hour, int minute)
+    {
+        TextView tv = (TextView) findViewById(R.id.TimeEditText);
+        tv.setText(hour + ":" + minute);
     }
 }
+
