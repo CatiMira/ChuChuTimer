c6a44007a3e8a38590802be87b8af0a0d9b78421
DeusArrow <dmytriy.pelts@bwdbern.ch>
2016-06-30 15:34:46 +0200

    End

--- app/src/main/java/com/example/bayrec/chuchutimer/NewAlarm.java
+++ app/src/main/java/com/example/bayrec/chuchutimer/NewAlarm.java
@@ -3,6 +3,9 @@ package com.example.bayrec.chuchutimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.regex.Pattern;
@@ -40,6 +43,45 @@ public class NewAlarm extends AppCompatActivity {
     private String[] splitDate(String text) {
         String[] temp = text.split(Pattern.quote("Dauer"));
         return temp[0].split(Pattern.quote(" - "));
-
     }
+
+    private String[] spliteTime(String text){
+
+        return text.split(Pattern.quote(":"));
+    }
+    public void saveOnClick(View v){
+        EditText et = (EditText) findViewById(R.id.NeedTime);
+        int value = Integer.valueOf(String.valueOf(et.getText()));
+
+        String[] temp =  spliteTime(StartOrtZeit);
+        int hour = Integer.valueOf(temp[0]);
+        int minute = Integer.valueOf(temp[1]);
+        minute += hour*60;
+        minute -= value;
+
+        hour = minute;
+        minute = minute % 60;
+        hour -= minute;
+        hour /=60;
+        String combineDate = combine(hour, minute);
+
+        // hour = hourCheck(hour);
+        // minute = minuteCheck(minute);
+    }
+
+    private String combine(int hour, int minute){
+        return hour+":"+minute;
+    }
+    /*
+    private int hourCheck(int hour){
+        if(hour < 0)
+            return 24 + hour;
+        return hour;
+    }
+    private int minuteCheck(int minute){
+        if(minute < 0)
+            return 60 + minute;
+        return minute;
+    }
+    */
 }
